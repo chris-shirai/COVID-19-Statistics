623fec7d73d80170e87b961d024c078712b17883
Loretac <LC@vt.edu>
2022-06-30 18:53:58 -0500

    Initial Commit

---
+++ COVID-19 Statistics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID-19 Statistics
+//
+//  Created by Chris Loreta on 6/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
