24e896e8d07b0b4915701b0f0706d3eaeee83769
Loretac <LC@vt.edu>
2022-08-04 22:42:29 -0500

    Cleanup

--- CovidAppIntents/IntentHandler.swift
+++ CovidAppIntents/IntentHandler.swift
@@ -16,15 +16,6 @@ class IntentHandler: INExtension, ShowCovidDataIntentHandling {
         let countryManager = CountryManager()
         var countryList: [SingleCountryIdentityData] = []
 
-        
-//        let ctries = countryManager.getCountryList()
-//
-//        let countryParams = ctries.map {CountryType(country: $0)}
-//
-//        completion(INObjectCollection(sections: [INObjectSection(title: "Countries", items: countryParams)])
-//                   , nil)
-        
-        
         let data = countryManager.readJSONFromFile(fileName: "countriesData")
         if let safeData = data {
             let sortedSafeData = safeData.sorted(by: { $0.display_name < $1.display_name })
@@ -34,20 +25,6 @@ class IntentHandler: INExtension, ShowCovidDataIntentHandling {
         let countryParams = countryList.map {CountryType(country: $0)}
 
         completion(INObjectCollection( items: countryParams), nil)
-        
-
-        
-        
-        let countries: [CountryType] = [
-            CountryType(identifier: "USA", display: "United States of America"),
-            CountryType(identifier: "UK", display: "United Kingdom")
-        ]
-        
-        // Create a collection with the array of characters.
-        let collection = INObjectCollection(items: countries)
-
-        // Call the completion handler, passing the collection.
-//        completion(collection, nil)
     }
     
 }
