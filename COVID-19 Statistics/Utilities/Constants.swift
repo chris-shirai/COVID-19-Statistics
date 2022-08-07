cd0707573b8989028115d23bee8472bae269a76e
Loretac <LC@vt.edu>
2022-08-07 15:19:03 -0500

    Updated alert messages when adding

--- COVID-19 Statistics/Utilities/Constants.swift
+++ COVID-19 Statistics/Utilities/Constants.swift
@@ -15,11 +15,10 @@ struct Constants{
 
 // Error list
 enum GFError: String, Error {
-    case invalidUsername    = "This username created an invalid request. Please try again."
     case unableToComplete   = "Unable to complete your request. Please check your internet connection"
     case invalidResponse    = "Invalid response from the server. Please try again."
     case invalidData        = "The data received from the server was invalid. Please try again."
-    case unableToFavorite   = "There was an error favoriting this user. Please try again."
-    case alreadyInFavorites = "You've already favorited this user. You must REALLY like them!"
+    case unableToFavorite   = "There was an error favoriting this country. Please try again."
+    case alreadyInFavorites = "Item is already in favorites"
 
 }
